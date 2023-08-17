2279fe71929a26b77da9bda8de21cbacaf26888c
allenkays <allenkays@tutanota.com>
2023-08-18 00:17:10 +0300

    Add simple flask app implementation

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Simple flask app implementation
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    """renders an html page"""
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
