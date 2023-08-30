6fd45880bf457b107e334c1c4c7a3fa525fd65e2
allenkays <allenkays@tutanota.com>
2023-08-31 00:03:38 +0300

    Create job processor

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,32 @@
+import kue from 'kue';
+
+const blacklistedNumbers = ['4153518780', '4153518781'];
+
+const sendNotification = (phoneNumber, message, job, done) => {
+  job.progress(0, 100);
+
+  if (blacklistedNumbers.includes(phoneNumber)) {
+    done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+  } else {
+    job.progress(50, 100);
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+    done();
+  }
+};
+
+const queue = kue.createQueue({
+  concurrency: 2, /* Process two jobs at a time */
+});
+
+queue.process('push_notification_code_2', 2, (job, done) => {
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message, job, done);
+});
+
+/* Gracefully handle process exit */
+process.on('SIGINT', () => {
+  queue.shutdown(5000, () => {
+    process.exit(0);
+  });
+});
+
