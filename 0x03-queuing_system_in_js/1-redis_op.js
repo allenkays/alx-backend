03011a171bdf35b92034c9f9f53cf547766a1288
allenkays <allenkays@tutanota.com>
2023-08-30 15:46:13 +0300

    Add function to set and display school key

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,37 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+  console.error(`Redis client not connected to the server: ${error}`);
+});
+
+const setNewSchool = (schoolName, value) => {
+  client.set(schoolName, value, redis.print);
+};
+
+const displaySchoolValue = (schoolName) => {
+  client.get(schoolName, (error, reply) => {
+    if (error) {
+      console.log(`Error getting value for ${schoolName}: ${error}`);
+    } else {
+      console.log(reply);
+    }
+  });
+};
+
+// Call the functions
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
+
+
+process.on('SIGINT', () => {
+  client.quit();
+  process.exit();
+});
+
